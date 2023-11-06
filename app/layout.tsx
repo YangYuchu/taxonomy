769a47b4fc3e2ef18260f6fdffed1ef6ce405f48
Yuchu Yang <yangyuchu0419@gmail.com>
2023-11-06 23:27:21 +0900

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import { Inter as FontSans } from "next/font/google"
 import localFont from "next/font/local"
 
+import { Analytics } from '@vercel/analytics/react';
+
 import "@/styles/globals.css"
 import { siteConfig } from "@/config/site"
 import { absoluteUrl, cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/toaster"
-import { Analytics } from "@/components/analytics"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
